ea2d83197a9793f3f793ec1bd7fff61ee61970f4
RichProsper <r.stp.p@hotmail.com>
2021-09-03 22:45:40 -0400

    Learning

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,15 +1,41 @@
 import React from 'react'
 
-export default function meetup_details() {
+export default function meetup_details({ meetupDetails }) {
     return (
-        <div>
+        <div id={meetupDetails.id}>
             <img
-                src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.gvn-zUVatq-nVTY4mnV8bAHaEK%26pid%3DApi&f=1'
-                alt='First Meetup' 
+                src={meetupDetails.image}
+                alt={meetupDetails.title}
             />
-            <h1>First Meetup</h1>
-            <address>Address</address>
-            <p>Meetup Description</p>
+            <h1>{meetupDetails.title}</h1>
+            <address>{meetupDetails.address}</address>
+            <p>{meetupDetails.description}</p>
         </div>
     )
+}
+
+export const getStaticPaths = async () => {
+    return {
+        fallback: false,
+        paths: [
+            { params: {meetupId: '1'} },
+            { params: {meetupId: '2'} }
+        ]
+    }
+}
+
+export const getStaticProps = async context => {
+    const id = context.params.meetupId
+
+    return {
+        props: {
+            meetupDetails: {
+                id,
+                image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.gvn-zUVatq-nVTY4mnV8bAHaEK%26pid%3DApi&f=1',
+                title: 'First Meetup',
+                address: '1234 Address',
+                description: 'Meetup Description'
+            }
+        }
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,30 @@
 import MeetupList from '../components/meetups/MeetupList'
 
-export default function index() {
-    const MEETUP_DATA = [
-        {
-            id: 1,
-            title: 'First Meet',
-            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.gvn-zUVatq-nVTY4mnV8bAHaEK%26pid%3DApi&f=1',
-            address: 'Address',
-            desc: 'Description'
-        },
-        {
-            id: 2,
-            title: 'Second Meet',
-            image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpoper.com%2Fimages%2F00%2F29%2F00%2F25%2Fforest_00290025.jpg&f=1&nofb=1',
-            address: 'Address',
-            desc: 'Description'
-        }
-    ]
+export default function index({ meetups }) {
+    return <MeetupList meetups={meetups} />
+}
 
-    return <MeetupList meetups={MEETUP_DATA} />
+export const getStaticProps = async () => {
+    // Fetch data from an API
+    return {
+        props: {
+            meetups: [
+                {
+                    id: 1,
+                    title: 'First Meet',
+                    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.gvn-zUVatq-nVTY4mnV8bAHaEK%26pid%3DApi&f=1',
+                    address: 'Address',
+                    desc: 'Description'
+                },
+                {
+                    id: 2,
+                    title: 'Second Meet',
+                    image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpoper.com%2Fimages%2F00%2F29%2F00%2F25%2Fforest_00290025.jpg&f=1&nofb=1',
+                    address: 'Address',
+                    desc: 'Description'
+                }
+            ]
+        },
+        revalidate: 60 //Rebuild and redeploy every 10 seconds
+    }
 }
